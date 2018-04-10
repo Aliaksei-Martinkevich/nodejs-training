f8063226bec0e4c68e085927bfe0e28ed99d2030
Aliaksei_Martsinkevich <Aliaksei_Martsinkevich@epam.com>
2018-04-10 17:13:05 +0300

    Import modules

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+import config from './config/config.json';
+import { Product, User } from './models';
 
-console.log('Hello world');
+console.log(`Application name: ${config.name}`);
+
+// eslint-disable-next-line no-unused-vars
+const user = new User();
+// eslint-disable-next-line no-unused-vars
+const product = new Product();
